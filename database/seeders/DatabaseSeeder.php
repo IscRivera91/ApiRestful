8cff59f75468fb638050d6007f07ba3b659b2625
Ricardo Rivera <ricardo@ingrivera.com>
2021-10-27 19:23:59 -0500

    RFX seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Product;
+use App\Models\Transaction;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +17,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+         User::factory(200)->create();
+         Category::factory(30)->create();
+         Product::factory(1000)->create();
+         Transaction::factory(1000)->create();
     }
 }
