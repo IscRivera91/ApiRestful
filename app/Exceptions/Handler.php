f9f1e5fa8c8bd1a046a58ff256ece96add330b76
Ricardo Rivera <ricardo@ingrivera.com>
2022-03-07 08:20:26 -0600

    Handler ready

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -6,14 +6,12 @@ use App\Traits\ApiResponser;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Illuminate\Database\MultipleRecordsFoundException;
-use Illuminate\Database\RecordsNotFoundException;
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-use Illuminate\Http\Exceptions\HttpResponseException;
-use Illuminate\Session\TokenMismatchException;
 use Illuminate\Validation\ValidationException;
-use Symfony\Component\HttpFoundation\Exception\SuspiciousOperationException;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
@@ -53,26 +51,51 @@ class Handler extends ExceptionHandler
 
         $this->renderable(function (Throwable $e) {
 
-            if ($e->getPrevious()) {
+            if ($e instanceof NotFoundHttpException && $e->getPrevious()) {
                 $e = $e->getPrevious();
             }
 
             if ($e instanceof ModelNotFoundException) {
-                return $this->errorResponse('Model not found',404);
+                return $this->errorResponse('Model not found',Response::HTTP_NOT_FOUND);
             }
 
             if ($e instanceof NotFoundHttpException) {
-                return $this->errorResponse('Bad request',404);
+                return $this->errorResponse('Url not found',Response::HTTP_NOT_FOUND);
             }
 
             if ($e instanceof ValidationException) {
                 $errors = $e->validator->errors()->getMessages();
-                return $this->errorResponse($errors,422);
+                return $this->errorResponse($errors,Response::HTTP_UNPROCESSABLE_ENTITY);
             }
 
             if ($e instanceof AuthenticationException) {
-                return $this->errorResponse($e->getMessage(),404);
+                return $this->errorResponse($e->getMessage(),Response::HTTP_FORBIDDEN);
             }
+
+            if ($e instanceof AuthorizationException) {
+                return $this->errorResponse('you don´t have permission to access',Response::HTTP_FORBIDDEN);
+            }
+
+            if ($e instanceof MethodNotAllowedHttpException) {
+                return $this->errorResponse('method not allowed',Response::HTTP_METHOD_NOT_ALLOWED);
+            }
+
+            if ($e instanceof HttpException) {
+                return $this->errorResponse($e->getMessage(),$e->getStatusCode());
+            }
+
+            if ($e instanceof QueryException) {
+                $mysqlCode = $e->errorInfo[1];
+                if ($mysqlCode == 1451) {
+                    return $this->errorResponse(
+                        'Integrity constraint violation: Cannot delete or update a parent row',
+                        Response::HTTP_CONFLICT
+                    );
+                }
+            }
+
+            return $this->errorResponse('internal server error',Response::HTTP_INTERNAL_SERVER_ERROR);
+
         });
     }
 
