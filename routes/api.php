eb00a024bfdb5d154f801fc8d1597793cf1d84d7
Ricardo Rivera <ricardo@ingrivera.com>
2022-03-26 21:39:54 -0600

    all api routes are plural

--- routes/api.php
+++ routes/api.php
@@ -24,6 +24,6 @@ use Illuminate\Support\Facades\Route;
 Route::resource('buyers',BuyerController::class, ['only' => ['index', 'show']]);
 Route::resource('categories',CategoryController::class, ['except' => ['create', 'edit']]);
 Route::resource('sellers',SellerController::class, ['only' => ['index', 'show']]);
-Route::resource('product',ProductController::class, ['only' => ['index', 'show']]);
-Route::resource('transaction',TransactionController::class, ['only' => ['index', 'show']]);
+Route::resource('products',ProductController::class, ['only' => ['index', 'show']]);
+Route::resource('transactions',TransactionController::class, ['only' => ['index', 'show']]);
 Route::resource('users',UserController::class, ['except' => ['create', 'edit']]);
